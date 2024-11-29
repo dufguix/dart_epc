ecc11b65356d76e8eabb0268d29d67d1a5ae9e8f
Guillaume Dufrene <duf.gui@gmail.com>
2024-11-29 16:56:04 +0100

    add common class method to EPC class
    add copyWith, toMap, fromMap, toJson, fromJson, toString, ==operator, hashCode

--- lib/src/epc.dart
+++ lib/src/epc.dart
@@ -1,4 +1,5 @@
-import "dart:convert" show utf8, latin1;
+import "dart:convert" show utf8, latin1, json;
+
 import "package:convert/convert.dart"
     show latin2, latin4, latinCyrillic, latinGreek, latin6, latin9;
 import 'dart:typed_data';
@@ -245,5 +246,113 @@ class Epc {
       throw EpcInvalidException("$val: $result", result);
     }
     return result;
+  }
+
+  Epc copyWith({
+    String? serviceTag,
+    Version? version,
+    CharSet? characterSet,
+    String? bic,
+    String? name,
+    String? iban,
+    String? amount,
+    String? purpose,
+    String? remittanceRef,
+    String? remittanceText,
+    String? information,
+    Separator? separator,
+  }) {
+    return Epc(
+      serviceTag: serviceTag ?? this.serviceTag,
+      version: version ?? this.version,
+      characterSet: characterSet ?? this.characterSet,
+      bic: bic ?? this.bic,
+      name: name ?? this.name,
+      iban: iban ?? this.iban,
+      amount: amount ?? this.amount,
+      purpose: purpose ?? this.purpose,
+      remittanceRef: remittanceRef ?? this.remittanceRef,
+      remittanceText: remittanceText ?? this.remittanceText,
+      information: information ?? this.information,
+      separator: separator ?? this.separator,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'serviceTag': serviceTag,
+      'version': version.name,
+      'characterSet': characterSet.name,
+      'bic': bic,
+      'name': name,
+      'iban': iban,
+      'amount': amount,
+      'purpose': purpose,
+      'remittanceRef': remittanceRef,
+      'remittanceText': remittanceText,
+      'information': information,
+      'separator': separator.name,
+    };
+  }
+
+  factory Epc.fromMap(Map<String, dynamic> map) {
+    return Epc(
+      serviceTag: map['serviceTag'] as String,
+      version: Version.values.byName(map['version']),
+      characterSet: CharSet.values.byName(map['characterSet']),
+      bic: map['bic'] as String,
+      name: map['name'] as String,
+      iban: map['iban'] as String,
+      amount: map['amount'] as String,
+      purpose: map['purpose'] as String,
+      remittanceRef: map['remittanceRef'] as String,
+      remittanceText: map['remittanceText'] as String,
+      information: map['information'] as String,
+      separator: Separator.values.byName(map['separator']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Epc.fromJson(String source) =>
+      Epc.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Epc(serviceTag: $serviceTag, version: $version, characterSet: $characterSet, bic: $bic, name: $name, iban: $iban, amount: $amount, purpose: $purpose, remittanceRef: $remittanceRef, remittanceText: $remittanceText, information: $information, separator: $separator)';
+  }
+
+  @override
+  bool operator ==(covariant Epc other) {
+    if (identical(this, other)) return true;
+
+    return other.serviceTag == serviceTag &&
+        other.version == version &&
+        other.characterSet == characterSet &&
+        other.bic == bic &&
+        other.name == name &&
+        other.iban == iban &&
+        other.amount == amount &&
+        other.purpose == purpose &&
+        other.remittanceRef == remittanceRef &&
+        other.remittanceText == remittanceText &&
+        other.information == information &&
+        other.separator == separator;
+  }
+
+  @override
+  int get hashCode {
+    return serviceTag.hashCode ^
+        version.hashCode ^
+        characterSet.hashCode ^
+        bic.hashCode ^
+        name.hashCode ^
+        iban.hashCode ^
+        amount.hashCode ^
+        purpose.hashCode ^
+        remittanceRef.hashCode ^
+        remittanceText.hashCode ^
+        information.hashCode ^
+        separator.hashCode;
   }
 }
